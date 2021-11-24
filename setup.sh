18f46aaf5923e9d64554e51f184e8c4bea893455
Kate Shulgina <shulgina@g.harvard.edu>
2021-11-24 17:50:10 -0500

    Fixed setup file

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Setup script for Codetta
+# Yekaterina Shulgina - Nov 2021
+
+# check that Codetta required Python packages are present
+python check_requirements.py
+
+# untar HMMER distribution
+tar xf hmmer.tar.gz
+
+# compile HMMER
+cd hmmer-3.3.2
+pwd | xargs -I {} ./configure --prefix={}
+make
+make install
+
+# compile Easel
+cd easel; make install
+cd ../..
+
+# download Pfam database built with --enone option
+# TBD
+
+# clean up
+# rm hmmer-3.3.2.tar.gz 
